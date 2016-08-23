802f8c4575bfaee482b88a611386fe9675e2b3b8
nelson <nyalsyon@gmail.com>
2016-08-23 16:08:19 +0545

    added the destroy article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,11 @@ class ArticlesController < ActionController::Base
   def index
     @article=Article.all
   end
+
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -15,9 +17,11 @@ class ArticlesController < ActionController::Base
         render 'new'
     end
   end
+
   def edit
     @article = Article.find(params[:id])
   end
+
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
@@ -32,9 +36,16 @@ class ArticlesController < ActionController::Base
     @article = Article.find(params[:id])
   end
 
+  def destroy
+
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :description)
-
     end
 end
