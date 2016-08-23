a19f47b1a2d7a05ea628d3fbeba99ad4c5670cc9
nelson <nyalsyon@gmail.com>
2016-08-23 12:50:11 +0545

    show action created and display the created article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,22 @@ class ArticlesController < ActionController::Base
   end
   def create
     @article=Article.new(article_params)
-    @article.save
+    if @article.save
+      flash[:notice] = "Article was successfully created."
+      redirect_to article_path(@article)
+    else
+
+        render 'new'
+    end
+  end
+
+  def show
+    @article=Article.find(params[:id])
   end
 
   private
     def article_params
       params.require(:article).permit(:title, :description)
-      redirect_to article_show(@article)
+
     end
 end
