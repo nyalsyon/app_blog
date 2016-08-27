da1db84c6ab10c96fb69aef3fe70ff0973c407e7
nelson <nyalsyon@gmail.com>
2016-08-27 14:13:32 +0545

    fixed the bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController < ActionController::Base
+class ArticlesController <ApplicationController
   before_action :set_article, only: [:edit,:update, :show, :destroy]
   def index
     @article=Article.all
