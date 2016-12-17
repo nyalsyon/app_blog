b9e05878f84ece16a2d56ef383ac190b2c8efd0b
nelson <nyalsyon@gmail.com>
2016-12-17 17:44:00 +0545

    aaded comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :comments, as: :commentable
   validates :title, presence: true, length: {maximun:100,minimum:6}
   validates :description, presence: true
   validates :user_id, presence:true
