0a7a1a70b6fa6322547500a94e5acf1380b11bb4
nelson <nyalsyon@gmail.com>
2016-12-20 23:10:58 +0545

    changed app name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_app_blog_session'
+Rails.application.config.session_store :cookie_store, key: '_Ten_Fingers_session'
